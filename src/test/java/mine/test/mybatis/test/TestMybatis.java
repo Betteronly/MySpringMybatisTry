a0f6729db3c8de7d424665dd88ed662e976d52be
Betteronly <roli_ice@163.com>
2016-04-18 15:57:47 +0800

    modify

--- src/test/java/mine/test/mybatis/test/TestMybatis.java
+++ src/test/java/mine/test/mybatis/test/TestMybatis.java
@@ -16,7 +16,42 @@ import mine.test.mybatis.domain.User;
 public class TestMybatis {
 
     public static void main(String[] args) throws IOException {
+        TestMybatis testMybatis = new TestMybatis();
 
+        testMybatis.testMainA();
+
+        testMybatis.testMybatisA();
+    }
+
+    public void testMybatisA() throws IOException {
+        // mybatis的配置文件
+        String resource = "conf-mybatis.xml";
+        // 使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
+        System.out.println(Test.class.getResource("/"));
+        InputStream is = Test.class.getClassLoader().getResourceAsStream(resource);
+        // 构建sqlSession的工厂
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
+
+        // 使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
+        // Reader reader = Resources.getResourceAsReader(resource);
+        // InputStream is = Resources.getResourceAsStream(resource);
+        // 构建sqlSession的工厂
+        // SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        // 创建能执行映射文件中sql的sqlSession
+
+        SqlSession session = sessionFactory.openSession();
+        /**
+         * 映射sql的标识字符串， me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值， getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
+         */
+        String statement = "mine.test.mybatis.mapping.UserMapper.getUser";// 映射sql的标识字符串
+        // 执行查询返回一个唯一user对象的sql
+        User user = session.selectOne(statement, 1);
+        System.out.println(user);
+
+        session.close();
+    }
+
+    public void testMainA() {
         String a = "11";
         final String b = "11";
         String c = a;
@@ -60,27 +95,5 @@ public class TestMybatis {
         map.put(2, 2);
         map.put(3, 3);
         System.out.println(map);
-
-        // mybatis的配置文件
-        String resource = "conf.xml";
-        // 使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
-        System.out.println(Test.class.getClassLoader().getResource(""));
-        System.out.println(Test.class.getResource("/"));
-        InputStream is = Test.class.getClassLoader().getResourceAsStream(resource);
-        // 构建sqlSession的工厂
-        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
-        // 使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
-        // Reader reader = Resources.getResourceAsReader(resource);
-        // 构建sqlSession的工厂
-        // SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
-        // 创建能执行映射文件中sql的sqlSession
-        SqlSession session = sessionFactory.openSession();
-        /**
-         * 映射sql的标识字符串， me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值， getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
-         */
-        String statement = "mine.test.mybatis.mapping.UserMapper.getUser";// 映射sql的标识字符串
-        // 执行查询返回一个唯一user对象的sql
-        User user = session.selectOne(statement, 1);
-        System.out.println(user);
     }
 }
